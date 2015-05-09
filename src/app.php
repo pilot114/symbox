efe8df7b4bf7883c21ad5f1955cf69acc6e1ca7d
oleg <pilot114@bk.ru>
2015-05-09 13:25:23 +0600

    HTTP Kernel

--- src/app.php
+++ src/app.php
@@ -11,16 +11,23 @@ function is_leap_year($year = null) {
     return 0 == $year % 400 || (0 == $year % 4 && 0 != $year % 100);
 }
 
-$routes = new Routing\RouteCollection();
-$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
-    'year' => null,
-    '_controller' => function ($request) {
-        if (is_leap_year($request->attributes->get('year'))) {
+class LeapYearController
+{
+	// HTTP Kernel feature: parameters, Request $request, in any order..
+    public function indexAction($year)
+    {
+        if (is_leap_year($year)) {
             return new Response('Yep, this is a leap year!');
         }
  
         return new Response('Nope, this is not a leap year.');
     }
-)));
+}
+
+$routes = new Routing\RouteCollection();
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', [
+    'year' => null,
+    '_controller' => 'LeapYearController::indexAction', // its lazy-load
+]));
  
 return $routes;
